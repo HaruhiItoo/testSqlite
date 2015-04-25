5e49595a7911b01a3eda0620f00edcb2de483773
HaruhiItoo <haruhi.itoo@gmail.com>
2015-04-25 23:16:47 +0800

    update button images

--- gen/com/example/m04surfaceviewtest/R.java
+++ gen/com/example/m04surfaceviewtest/R.java
@@ -16,20 +16,27 @@ public final class R {
         public static final int selectedsch=0x7f050002;
     }
     public static final class drawable {
-        public static final int alarm=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int logo1=0x7f020002;
-        public static final int logo2=0x7f020003;
-        public static final int noalarm=0x7f020004;
+        public static final int addsch=0x7f020000;
+        public static final int alarm=0x7f020001;
+        public static final int deletesch=0x7f020002;
+        public static final int editsch=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logo1=0x7f020005;
+        public static final int logo2=0x7f020006;
+        public static final int noalarm=0x7f020007;
     }
     public static final class id {
-        public static final int lv=0x7f070000;
+        public static final int ibAddSch=0x7f070000;
+        public static final int ibDelSch=0x7f070001;
+        public static final int ibEditSch=0x7f070002;
+        public static final int lv=0x7f070003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int curSchedules=0x7f040003;
         /**  
     <string name="hello_world">Hello world!</string>
      
